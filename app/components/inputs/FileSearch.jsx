36f5150855ec5b4557f0855ac21f144ef2077e40
Chris Reed <creed2@its.jnj.com>
2016-10-13 12:26:00 -0400

    Playing with scroll stuff

--- app/components/inputs/FileSearch.jsx
+++ app/components/inputs/FileSearch.jsx
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import React from 'react'
+import React, { Component } from 'react'
 import ui from 'redux-ui'
 
 import Card from '../stylize/Card.jsx'
@@ -19,56 +19,72 @@ const tryResults = (value, previousResults) => {
 	return results
 }
 
-const FileSearch = ({ ui, updateUI }) => <Card>
-	<TextInput
-		autoFocus={true}
-		className={styles.TextInput}
-		value={ui.valueToSearch}
-		onChange={
-			e => {
-				const findResults = value => tryResults(
-					value,
-					ui.results
-				).filter( // remove all private files
-					fileName => !fileName.match(/^\./)
-				).filter(
-					// filter the strings that contain at least one char from the value
-					// TODO filter the strings that contain all of the letters from value
-					fileName => !!value.match(/\/$/) ||
-						fileName.split('').map(
-							letter => path.parse(value).base.split('').includes(letter)
-						).reduce(
-							(p,c) => p || c, false
-						)
-				)
-				const results = findResults(e.currentTarget.value)
+const handleScroll = e => console.log(e)
 
-				const valueToSearch = results.length === 1 ?
-					`${path.parse(e.currentTarget.value).dir}/${results[0]}/`.replace('//', '/') :
-					e.currentTarget.value
-
-				updateUI({
-					valueToSearch: tryResults(valueToSearch) ? valueToSearch : e.currentTarget.value,
-					results: findResults(valueToSearch)
-				})
-			}
-		}
-	/>
-<List>
-		{
-			ui.results.map(
-				result => <ListItem key={result}>{ result }</ListItem>
-			)
-		}
-	</List>
-</Card>
-
-const FileSearchUi = ui({
+@ui({
 	key: 'FileSearch',
 	state: {
 		valueToSearch: '',
 		results: []
 	}
-})(FileSearch)
+})
 
-export default FileSearchUi
+class FileSearch extends Component {
+	componentDidMount() {
+		// TODO better element selection
+		document.querySelector('#some-id').addEventListener('scroll', handleScroll);
+	}
+
+	componentWillUnmount() {
+		// TODO better element selection
+		document.querySelector('#some-id').removeEventListener('scroll', handleScroll);
+	}
+
+	render() {
+		const { ui, updateUI } = this.props
+		return <Card id='some-id' className={styles.Card}>
+			<TextInput
+				autoFocus={true}
+				className={styles.TextInput}
+				value={ui.valueToSearch}
+				onChange={
+					e => {
+						const findResults = value => tryResults(
+							value,
+							ui.results
+						).filter( // remove all private files
+							fileName => !fileName.match(/^\./)
+						).filter(
+							// filter the strings that contain at least one char from the value
+							fileName => !!value.match(/\/$/) ||
+								fileName.split('').filter(
+									letter => path.parse(value).base.split('').includes(letter)
+								).length >= path.parse(value).base.length
+						)
+						const results = findResults(e.currentTarget.value)
+
+						const valueToSearch = results.length === 1 ?
+							`${path.parse(e.currentTarget.value).dir}/${results[0]}/`.replace('//', '/') :
+							e.currentTarget.value
+
+						updateUI({
+							valueToSearch: tryResults(valueToSearch) ? valueToSearch : e.currentTarget.value,
+							results: findResults(valueToSearch)
+						})
+					}
+				}
+			/>
+		<List>
+				{
+					ui.results.map(
+						result => <ListItem key={result}>{ result }</ListItem>
+					)
+				}
+			</List>
+		</Card>
+	}
+}
+
+
+
+export default FileSearch
--- app/components/stylize/Card.jsx
+++ app/components/stylize/Card.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 import styles from './Card.css'
 import Shadow from './Shadow.jsx'
 
-const Card = ({ children }) => <Shadow force={true}>
-  <div className={styles.Card}>{ children }</div>
+const Card = ({ children, className, ...props }) => <Shadow
+	force={true}
+>
+  <div
+		{ ...props }
+		className={[
+			styles.Card,
+			className
+		].join(' ')}
+	>{ children }</div>
 </Shadow>
 
 export default Card

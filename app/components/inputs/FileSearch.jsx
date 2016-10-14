edadeb2ef1905f7553f9353a319747fe64d47590
Chris Reed <creed2@its.jnj.com>
2016-10-14 19:21:16 -0400

    Added key board support for file search
    also updated styles

--- app/components/inputs/FileSearch.jsx
+++ app/components/inputs/FileSearch.jsx
@@ -12,6 +12,9 @@ import ListItem from '../typeography/ListItem'
 import styles from './FileSearch.css'
 
 const tryResults = (value, previousResults) => {
+	if (!value.match(/\/$/)) {
+		return previousResults
+	}
 	let results = previousResults
 	try {
 		results = fs.readdirSync(value)
@@ -19,72 +22,124 @@ const tryResults = (value, previousResults) => {
 	return results
 }
 
-const handleScroll = e => console.log(e)
+const findResults = (value, previousResults) => tryResults(
+	value,
+	previousResults
+).filter( // remove all private files
+	fileName => !fileName.match(/^\./)
+).filter(
+	// filter the strings that contain at least one char from the value
+	fileName => !!value.match(/\/$/) ||
+		fileName.split('').filter(
+			letter => path.parse(value).base.split('').includes(letter)
+		).length >= path.parse(value).base.length
+)
 
-@ui({
+const FileSearch = ({ ui, updateUI }) => <Card
+	className={styles.Card}
+	onKeyDown={
+		e => {
+			const selectedResultIndexDictionary = {// look up the key pressed and handle it,
+				// press down
+				[40]: ui.selectedResultIndex < ui.results.length - 1 ?
+					ui.selectedResultIndex + 1 :
+					ui.selectedResultIndex,
+				// press up
+				[38]: ui.selectedResultIndex > 1 ?
+					ui.selectedResultIndex - 1 :
+					0
+			}
+			const lookupResultIndex = selectedResultIndexDictionary[e.keyCode]
+			const valueToSearchDictionary = {
+				// tab
+				[9]: `${path.parse(ui.valueToSearch).dir}/${ui.results[ui.selectedResultIndex]}/`.replace('//', '/')
+			}
+			const lookupValueToSearch = valueToSearchDictionary[e.keyCode]
+
+			if (
+				Object.getOwnPropertyNames({
+					...selectedResultIndexDictionary,
+					...valueToSearchDictionary
+				}).includes(
+					e.keyCode.toString()
+				)
+			) {
+				e.preventDefault()
+			}
+
+			return updateUI({
+				valueToSearch: lookupValueToSearch || ui.valueToSearch,
+				results: lookupValueToSearch ?
+					findResults(lookupValueToSearch, ui.results):
+					ui.results,
+				selectedResultIndex: lookupResultIndex === 0 ?
+					lookupResultIndex :
+					lookupResultIndex || ui.selectedResultIndex
+			})
+		}
+	}
+>
+	<TextInput
+		autoFocus={true}
+		className={styles.TextInput}
+		value={ui.valueToSearch}
+		onChange={
+			e => {
+
+				const results = findResults(e.currentTarget.value, ui.results)
+
+				const valueToSearch = results.length === 1 ?
+					`${path.parse(e.currentTarget.value).dir}/${results[0]}/`.replace('//', '/') :
+					e.currentTarget.value
+
+				updateUI({
+					valueToSearch: tryResults(valueToSearch) ? valueToSearch : e.currentTarget.value,
+					results: findResults(valueToSearch, ui.results)
+				})
+			}
+		}
+	/>
+<List>
+		{
+			ui.results.map(
+				(result, i) => <ListItem
+					className={[
+						i === ui.selectedResultIndex ? styles.selectedResult : '',
+						styles.clickable
+					].join(' ')}
+					key={result}
+					onClick={
+						e => {
+							const parsed = path.parse(ui.valueToSearch)
+							const dir = parsed.dir
+							const base = ui.valueToSearch.match(/\/$/) ? parsed.base : ''
+							console.log({
+								parsed,
+								dir,
+								base
+							})
+							const valueToSearch = `${dir}/${base}/${result}/`.replace(/\/+/g, '/')
+							const results = findResults(valueToSearch, ui.results)
+							updateUI({
+								valueToSearch,
+								results
+							})
+						}
+					}
+				>{ result }</ListItem>
+			)
+		}
+	</List>
+</Card>
+
+const FileSearchUi = ui({
 	key: 'FileSearch',
 	state: {
 		valueToSearch: '',
-		results: []
+		results: [],
+		selectedResultIndex: 0
 	}
-})
-
-class FileSearch extends Component {
-	componentDidMount() {
-		// TODO better element selection
-		document.querySelector('#some-id').addEventListener('scroll', handleScroll);
-	}
-
-	componentWillUnmount() {
-		// TODO better element selection
-		document.querySelector('#some-id').removeEventListener('scroll', handleScroll);
-	}
-
-	render() {
-		const { ui, updateUI } = this.props
-		return <Card id='some-id' className={styles.Card}>
-			<TextInput
-				autoFocus={true}
-				className={styles.TextInput}
-				value={ui.valueToSearch}
-				onChange={
-					e => {
-						const findResults = value => tryResults(
-							value,
-							ui.results
-						).filter( // remove all private files
-							fileName => !fileName.match(/^\./)
-						).filter(
-							// filter the strings that contain at least one char from the value
-							fileName => !!value.match(/\/$/) ||
-								fileName.split('').filter(
-									letter => path.parse(value).base.split('').includes(letter)
-								).length >= path.parse(value).base.length
-						)
-						const results = findResults(e.currentTarget.value)
-
-						const valueToSearch = results.length === 1 ?
-							`${path.parse(e.currentTarget.value).dir}/${results[0]}/`.replace('//', '/') :
-							e.currentTarget.value
-
-						updateUI({
-							valueToSearch: tryResults(valueToSearch) ? valueToSearch : e.currentTarget.value,
-							results: findResults(valueToSearch)
-						})
-					}
-				}
-			/>
-		<List>
-				{
-					ui.results.map(
-						result => <ListItem key={result}>{ result }</ListItem>
-					)
-				}
-			</List>
-		</Card>
-	}
-}
+})(FileSearch)
 
 
-
-export default FileSearch
+export default FileSearchUi
--- app/components/typeography/ListItem.jsx
+++ app/components/typeography/ListItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import styles from './ListItem.css'
 
-const ListItem = ({ children, ...props }) => <li
+const ListItem = ({ children, className, ...props }) => <li
 	{...props}
-	className={styles.ListItem}
+	className={`${styles.ListItem} ${className}`}
 >{ children }</li>
 
 export default ListItem
